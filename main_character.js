8c54a3e747f7c75371b4f8c9360c3ce6880e985a
William Harding <william.jeffrey.harding@gmail.com>
2015-10-27 07:41:23 -0700

    added tooltip
    tool tip for main character added - no data file yet.  (also add d3.js
    to run locally)

--- main_character.js
+++ main_character.js
@@ -1,8 +1,22 @@
+
+var char_tooltip = d3.select("body")
+	.append("div")
+	.style("position", "absolute")
+	.style("z-index", "10")
+	.style("visibility", "hidden")
+	.html("player character<br>other info<br>other info");
+	
 var player_character = canvas.append("circle")
 							.attr("cx", char_x)
 							.attr("cy", char_y)
-							.attr("r", 20);
+							.attr("r", 5)
+							.on("mouseover", function(){return char_tooltip.style("visibility", "visible");})
+							.on("mousemove", function(){return char_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+							.on("mouseout", function(){return char_tooltip.style("visibility", "hidden");});
+	
 
+	
+	
 player_character.moveUp = function(y){
 	char_y = char_y - y
 	this.transition()
@@ -26,4 +40,6 @@ player_character.moveRight = function(x){
 	this.transition()
 		.attr("cx", char_x)	
 }
+
+
 							
