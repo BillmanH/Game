86b4cc24028b413e00d1c2cba85c77a57931e5ae
William Harding <william.jeffrey.harding@gmail.com>
2015-10-28 07:45:09 -0700

    zombies not moving
    I need to pull the data out of each object with the wander function.
    Zombies are placed on screen but not moving.

--- zombies.js
+++ zombies.js
@@ -1,45 +1,65 @@
 
-var char_tooltip = d3.select("body")
+
+function get_random_screen_position(max,num){
+	var l_marg = 10,
+		u_marg = max-10,
+		nums = [];
+	for(var i=0;i<=num;i++){
+		do {
+			tmp = Math.floor(Math.random() * max)
+		} while (tmp > u_marg && tmp < l_marg);
+		nums.push(tmp)
+	}
+	return nums;
+}	
+
+var num_zombies = 10;
+var zombie_x =get_random_screen_position(width,num_zombies);
+var zombie_y = get_random_screen_position(height,num_zombies);
+zombie_data = []
+for(var i=0;i<=num_zombies;i++){
+	item = {};
+	item["name"]="zombie";
+	item["x"] = zombie_x[i];
+	item["y"] = zombie_y[i];
+	zombie_data.push(item);
+}
+console.log(zombie_data);
+// ;return d["y"]
+
+var zombie_tooltip = d3.select("body")
 	.append("div")
 	.style("position", "absolute")
 	.style("z-index", "10")
-	.style("visibility", "hidden")
-	.html("player character<br>other info<br>other info");
+	.style("visibility", "hidden");
 	
-var player_character = canvas.append("circle")
-							.attr("cx", char_x)
-							.attr("cy", char_y)
-							.attr("r", 5)
-							.on("mouseover", function(){return char_tooltip.style("visibility", "visible");})
-							.on("mousemove", function(){return char_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-							.on("mouseout", function(){return char_tooltip.style("visibility", "hidden");});
+var zombies = canvas.selectAll("circle")
+					.data(zombie_data)
+					.enter()
+						.append("circle")   
+						.attr("r",5)
+						.attr("cy", function(d){return d["y"]})
+						.attr("cx", function(d){return d["x"]})
+						.style("fill", "rgb(255,0,0)")
+						.on("mouseover", function(d){return zombie_tooltip.style("visibility", "visible")
+																		.html("creature: " + d["name"] + "<br>y: " + d["y"] + "<br>x: " + d["x"]);})
+						.on("mousemove", function(){return zombie_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+						.on("mouseout", function(){return zombie_tooltip.style("visibility", "hidden");})
+					
 	
 
+function wander(d){
+	var creature = d3.select(this);
+	(function repeat(d){
+		direction = Math.floor(Math.random() * 4 + 1)
+		if (direction==1){creature.transition().attr("cx", d["x"] + 5).each("end",repeat)}
+		if (direction==2){creature.transition().attr("cx", d["x"] - 5).each("end",repeat)}
+		if (direction==3){creature.transition().attr("cx", d["y"] + 5).each("end",repeat)}
+		if (direction==4){creature.transition().attr("cx", d["y"] - 5).each("end",repeat)}
+		console.log(direction);
+	})
+
+}
 	
 	
-player_character.moveUp = function(y){
-	char_y = char_y - y
-	this.transition()
-		.attr("cy", char_y)	
-}
-
-player_character.moveDown = function(y){
-	char_y = char_y + y
-	this.transition()
-		.attr("cy", char_y)	
-}
-
-player_character.moveLeft = function(x){
-	char_x = char_x - x
-	this.transition()
-		.attr("cx", char_x)	
-}
-
-player_character.moveRight = function(x){
-	char_x = char_x + x
-	this.transition()
-		.attr("cx", char_x)	
-}
-
-
-							
+d3.selectAll(zombies).data(zombie_data).each(wander())
