da73a3cd10656e2e33b1727b8eb011a2d66c9573
William Harding <william.jeffrey.harding@gmail.com>
2015-11-04 08:09:06 -0800

    Tiles
    lots of work on the tiles, lots to go

--- DungeonCrawl/make_tile_json.py
+++ DungeonCrawl/make_tile_json.py
@@ -3,11 +3,14 @@ import numpy as np
 import json
 
 #step one: defining matrix values
-screen_lenght = 1200
+screen_height = 1200
 screen_width = 600
 
-aspect = [50,25] #this is the number of cells that you want to have on the screen
+aspect = [30,60] #y:height / x:width
 
-x_values = [screen_lenght-(screen_lenght-(screen_lenght/aspect[0]*x)) for x in range(aspect[0])]
-y_values = [screen_width-(screen_width-(screen_width/aspect[1]*x)) for x in range(aspect[1])]
-world_matrix =[[[x,y] for x in x_values] for y in y_values]
+y_values = [screen_height-(screen_height-(screen_height/aspect[0]*x)) for x in range(aspect[0])]
+x_values = [screen_width-(screen_width-(screen_width/aspect[1]*x)) for x in range(aspect[1])]
+world_matrix =[[(x,y) for x in y_values] for y in x_values]
+
+map_grid = {}
+ma
